5e12ed7227f356cac6f58533e36cd94ace0a317d
xzh <xzh_xuzhonghui@163.com>
2019-04-29 14:22:16 +0800

    add junitTest

---
+++ src/main/java/com/xzh/customer/junitTest/Mytest.java
@@ -0,0 +1,13 @@
+package com.xzh.customer.junitTest;
+
+/**
+ * @author XZHH
+ * @Description:
+ * @create 2019/4/29 0029 14:19
+ * @modify By:
+ **/
+public class Mytest {
+    public void soutTest(){
+        System.out.println("Junit测试");
+    }
+}
---
+++ src/test/java/com/xzh/customer/junitTest/MytestTest.java
@@ -0,0 +1,46 @@
+package com.xzh.customer.junitTest;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author XZHH
+ * @Description:
+ * @create 2019/4/29 0029 14:19
+ * @modify By:
+ **/
+public class MytestTest {
+    //无法注入
+    @Autowired
+    private Mytest mytest;
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void soutTest() throws Exception {
+        mytest.soutTest();
+    }
+
+    @Test(timeout = 1000)   //超过1s就不通过
+    public void testTimeout() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("Complete");
+    }
+
+    @Test(expected = NullPointerException.class)    //不抛NullPointerException就不通过
+    public void testNullException() {
+        throw new NullPointerException();
+    }
+
+}
