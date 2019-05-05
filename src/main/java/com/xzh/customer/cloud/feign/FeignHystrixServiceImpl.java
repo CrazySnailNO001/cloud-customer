e428c322effe9fa800620c9265b494a3f33d85f9
xzh <xzh_xuzhonghui@163.com>
2019-05-05 16:20:23 +0800

    add hystrix

---
+++ src/main/java/com/xzh/customer/cloud/feign/FeignHystrixServiceImpl.java
@@ -0,0 +1,28 @@
+package com.xzh.customer.cloud.feign;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author XZHH
+ * @Description:
+ * @create 2019/5/5 0005 15:20
+ * @modify By:
+ **/
+@Component
+public class FeignHystrixServiceImpl implements FeignHystrixService {
+
+    @Override
+    public String testTimeOutFeign(Long time) {
+        return "testTimeOutFeign请求失败,进入fallback";
+    }
+
+    /**
+     * hystrix fallback方法
+     * @param name name
+     * @return 默认的用户
+     */
+    @Override
+    public String hello(String name) {
+        return "hello请求失败,进入fallback";
+    }
+}
