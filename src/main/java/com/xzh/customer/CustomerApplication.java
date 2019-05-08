7f3ea5c984be9abbc1aa809eaad8cfaf66fa71d7
xzh <xzh_xuzhonghui@163.com>
2019-05-08 11:13:00 +0800

    add BitOperation

--- src/main/java/com/xzh/customer/CustomerApplication.java
+++ src/main/java/com/xzh/customer/CustomerApplication.java
@@ -9,13 +9,12 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
-//@SpringBootApplication
-//@EnableEurekaClient
-//@EnableCircuitBreaker
-@SpringCloudApplication  //相当于上面3个的注解
+@SpringCloudApplication  //相当于SpringBootApplication EnableEurekaClient EnableCircuitBreaker3个的注解
 @EnableFeignClients(basePackages = {"com.xzh.customer.cloud.feign"})
+@EnableTransactionManagement //开启事务
 @ComponentScan("com.xzh.customer")
 public class CustomerApplication {
 
---
+++ src/main/java/com/xzh/customer/operation/BitOperation.java
@@ -0,0 +1,47 @@
+package com.xzh.customer.operation;
+
+/**
+ * @author XZHH
+ * @Description:
+ * @create 2019/5/8 0008 10:18
+ * @modify By:
+ **/
+public class BitOperation {
+
+    public static void main(String[] args) {
+        int a = 10;
+        printInfo(a);
+
+        //左移一位(不分正负数，低位补0；)
+        printInfo(a<<1);
+
+        //右移2位(如果该数为正，则高位补0，若为负数，则高位补1；)
+        printInfo(a>>2);
+
+        printInfo(-a);
+
+        //右移2位(负数最高位用1补齐)
+        printInfo(-a>>2);
+
+        //无符号右移2位(忽略符号位，空位都以0补齐)
+        printInfo(-a>>>2);
+        /*
+          -20的源码：10000000  00000000   00000000   00010100
+
+        　　　　反码：11111111  11111111   11111111   11101011
+
+        　　　　补码：11111111  11111111   11111111   11101100
+
+       无符号右移2位：00111111  11111111   11111111   11111011
+        * */
+
+    }
+
+    /**
+     * 输出一个int的二进制数
+     * @param num
+     */
+    private static void printInfo(int num) {
+        System.out.println(Integer.toBinaryString(num));
+    }
+}
