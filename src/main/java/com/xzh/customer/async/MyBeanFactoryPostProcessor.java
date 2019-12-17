92ee0a036252871bd45456a46f2e4718432a2c48
Zhonghui XU <zhonghui.xu.partner@decathlon.com>
2019-12-17 11:01:09 +0800

    modify

--- src/main/java/com/xzh/customer/async/MyBeanFactoryPostProcessor.java
+++ src/main/java/com/xzh/customer/async/MyBeanFactoryPostProcessor.java
@@ -1,4 +1,4 @@
-package com.xzh.customer.config;
+package com.xzh.customer.async;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanDefinition;
