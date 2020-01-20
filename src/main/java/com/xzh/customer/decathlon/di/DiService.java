f0658e51ea9c961f848bed63d7b7b079c84e7b8f
Zhonghui XU <zhonghui.xu.partner@decathlon.com>
2020-01-20 17:05:40 +0800

    DI

---
+++ src/main/java/com/xzh/customer/decathlon/di/DiService.java
@@ -0,0 +1,18 @@
+package com.xzh.customer.decathlon.di;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author ：xzh
+ * @date ：Created in 2020-01-16 15:22
+ * @description：
+ * @modified By：
+ * @version:
+ */
+@Service
+public class DiService {
+    public String test001(String result){
+        System.out.println(result);
+        return result;
+    }
+}
