4a23bce38fe832ba2ec6d77eb45cca0a59331071
Zhonghui XU <zhonghui.xu.partner@decathlon.com>
2020-02-21 17:26:56 +0800

    add json

--- src/main/java/com/xzh/customer/decathlon/fastJson/FastJasonDto.java
+++ src/main/java/com/xzh/customer/decathlon/fastJson/FastJasonDto.java
@@ -1,9 +1,17 @@
 package com.xzh.customer.decathlon.fastJson;
 
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.databind.annotation.JsonTypeIdResolver;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * @author ：xzh
@@ -13,10 +21,37 @@ import java.io.Serializable;
  * @version:
  */
 
-@Data
+@NoArgsConstructor
 @AllArgsConstructor
+@ApiModel
+@Data
+@JsonTypeIdResolver(LowerCaseClassNameResolver.class)
+@JsonAutoDetect(getterVisibility = JsonAutoDetect.Visibility.ANY) //任何级别的字段都可以自动识别
+@JsonInclude(JsonInclude.Include.NON_NULL)//解决实体类与json互转的时候 属性值为null的不参与序列化
+@JsonIgnoreProperties(ignoreUnknown = true) //过滤掉未知的属性
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)//json序列化策略 驼峰
 public class FastJasonDto implements Serializable {
 
     private static final long serialVersionUID = -3684100780450249163L;
 
+    //    @JsonProperty("user_name") 解析后格式为：{"user_name":"张三"}
+    @ApiModelProperty("用户姓名")
+    private String userName;
+
+    @ApiModelProperty("用户年龄")
+    private String userAge;
+
+    /**
+     * 格式化日期属性
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    @ApiModelProperty("用户生日")
+    private Date birthday;
+
+    /**
+     * 不JSON 序列化此属性
+     */
+    @JsonIgnore
+    @ApiModelProperty("别名")
+    private String orgName;
 }
---
+++ src/main/java/com/xzh/customer/decathlon/fastJson/FastJsonController.java
@@ -0,0 +1,37 @@
+package com.xzh.customer.decathlon.fastJson;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
+/**
+ * @author ：xzh
+ * @date ：Created in 2020-02-21 16:13
+ * @description：
+ * @modified By：
+ * @version:
+ */
+@RestController
+@RequestMapping("/json")
+@Slf4j
+public class FastJsonController {
+
+    @GetMapping("/test001")
+    public FastJasonDto test001() {
+        FastJasonDto fastJasonDto = new FastJasonDto();
+        fastJasonDto.setUserName("张三");
+        fastJasonDto.setUserAge("12");
+        fastJasonDto.setBirthday(new Date());
+        fastJasonDto.setOrgName("没我");
+
+        return fastJasonDto;
+    }
+
+    @PostMapping(value = "/test002", produces = {"application/json"}, consumes = {"application/json"})
+    public FastJasonDto test002(@RequestBody FastJasonDto fastJasonDto) {
+        fastJasonDto.setBirthday(new Date());
+
+        return fastJasonDto;
+    }
+}
