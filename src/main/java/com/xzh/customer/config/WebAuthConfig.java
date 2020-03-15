bd276dacdf60deabc937453863729026a4443c8b
Zhonghui XU <zhonghui.xu.partner@decathlon.com>
2020-03-15 20:03:49 +0800

    modify WebAuthConfig

--- src/main/java/com/xzh/customer/config/WebAuthConfig.java
+++ src/main/java/com/xzh/customer/config/WebAuthConfig.java
@@ -3,6 +3,7 @@ package com.xzh.customer.config;
 import com.xzh.customer.decathlon.authInterceptor.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import javax.annotation.Resource;
@@ -22,6 +23,13 @@ public class WebAuthConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor).addPathPatterns("/auth/**");
-//        super.addInterceptors(registry);
+        super.addInterceptors(registry);
+    }
+
+    //由于继承WebMvcConfigurationSupport后会导致自动配置失效，所以这里要指定默认的静态资源的位置。
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+        super.addResourceHandlers(registry);
     }
 }
