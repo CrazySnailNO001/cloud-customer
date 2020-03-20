ec08952a6ac3043a899836ae2c4027c33aabd4fc
Zhonghui XU <zhonghui.xu.partner@decathlon.com>
2020-03-20 16:17:51 +0800

    modify

--- src/main/java/com/xzh/customer/config/FilterConfig.java
+++ src/main/java/com/xzh/customer/config/FilterConfig.java
@@ -1,5 +1,12 @@
 package com.xzh.customer.config;
 
+import com.xzh.customer.decathlon.filter.MyTestFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
 /**
  * @author ：xzh
  * @date ：Created in 2020-03-15 20:37
@@ -7,18 +14,18 @@ package com.xzh.customer.config;
  * @modified By：
  * @version:
  */
-//@Configuration
-//public class FilterConfig {
-//
-//    @Resource
-//    private MyTestFilter myTestFilter;
-//
-//    @Bean
-//    public FilterRegistrationBean testFilterRegistration() {
-//        FilterRegistrationBean registration = new FilterRegistrationBean(myTestFilter);
-//        registration.addUrlPatterns("/filter/*");
-//        registration.setName("testFilter");
-//        registration.setOrder(1);
-//        return registration;
-//    }
-//}
+@Configuration
+public class FilterConfig {
+
+    @Resource
+    private MyTestFilter myTestFilter;
+
+    @Bean
+    public FilterRegistrationBean testFilterRegistration() {
+        FilterRegistrationBean registration = new FilterRegistrationBean(myTestFilter);
+        registration.addUrlPatterns("/filter/*");
+        registration.setName("testFilter");
+        registration.setOrder(1);
+        return registration;
+    }
+}
