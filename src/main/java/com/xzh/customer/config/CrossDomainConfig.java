c54d69748361321b3a9cd4f73acdc5fa418ecbe5
Zhonghui XU <zhonghui.xu.partner@decathlon.com>
2020-04-13 16:25:49 +0800

    add crossDomain

---
+++ src/main/java/com/xzh/customer/config/CrossDomainConfig.java
@@ -0,0 +1,32 @@
+package com.xzh.customer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * @author ：xzh
+ * @date ：Created in 2020-04-13 20:37
+ * @description：
+ * @modified By：解决跨域问题
+ * @version:
+ */
+@Configuration
+public class CrossDomainConfig {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); // allow any damian name
+        corsConfiguration.addAllowedHeader("*"); // allow any custom header
+        corsConfiguration.addAllowedMethod("*"); // allow any request method（post、get, put, delete,option）
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // enable all interface support cross domian
+        return new CorsFilter(source);
+    }
+}
