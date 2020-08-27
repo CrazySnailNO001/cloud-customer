cbda98b5a2c9fc2154032e6ae68cbd911cd1010b
Zhonghui XU <zhonghui.xu.partner@decathlon.com>
2020-08-27 16:23:27 +0800

    add condition

---
+++ src/main/java/com/xzh/customer/technical/boot/MyConditionBeanConfig.java
@@ -0,0 +1,77 @@
+package com.xzh.customer.technical.boot;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ：xzh
+ * @date ：Created in 2020-08-24 17:28
+ * @description：
+ * @modified By：
+ * @version:
+ */
+@Configuration
+public class MyConditionBeanConfig {
+    @Bean
+    public Demo Demo() {
+        return new Demo();
+    }
+
+    @Bean
+    @ConditionalOnBean(Demo.class)//有Bean就加载
+    public ClassAA classAA() {
+        return new ClassAA();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(Demo.class)//没有Bean就加载
+    public ClassBB ClassBB() {
+        return new ClassBB();
+    }
+
+    @Bean
+    @ConditionalOnClass(Demo.class) //有class就加载
+    public ClassC classC() {
+        return new ClassC();
+    }
+
+    @Bean
+    @ConditionalOnMissingClass("com.xzh.customer.technical.boot.MyConditionBeanConfig") //没有class就加载
+    public ClassD classD() {
+        return new ClassD();
+    }
+
+    @Bean
+    @ConditionalOnMissingClass("com.xzh.customer.technical.boot.MyConditionBeanConfig.NoThisBean.class")
+    public ClassE classE() {
+        return new ClassE();
+    }
+
+    private static class Demo {
+
+    }
+
+    public static class ClassAA {
+
+    }
+
+    public static class ClassBB {
+
+    }
+
+    public static class ClassC {
+
+    }
+
+    public static class ClassD {
+
+    }
+
+    public static class ClassE {
+
+    }
+}
---
+++ src/test/java/com/xzh/customer/technical/ConditionTest.java
@@ -0,0 +1,44 @@
+package com.xzh.customer.technical;
+
+import com.xzh.customer.CustomerApplication;
+import com.xzh.customer.technical.boot.MyConditionBeanConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+/**
+ * @author ：xzh
+ * @date ：Created in 2020-08-24 17:37
+ * @description：
+ * @modified By：
+ * @version:
+ */
+@SpringBootTest(classes = CustomerApplication.class)
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@Slf4j
+public class ConditionTest {
+    @Autowired(required = false)
+    private MyConditionBeanConfig.ClassAA classA;
+    @Autowired(required = false)
+    private MyConditionBeanConfig.ClassBB classB;
+    @Autowired(required = false)
+    private MyConditionBeanConfig.ClassC classC;
+    @Autowired(required = false)
+    private MyConditionBeanConfig.ClassD classD;
+    @Autowired(required = false)
+    private MyConditionBeanConfig.ClassE classE;
+
+    @Test
+    public void test() {
+        log.info("classA: {}", classA);
+        log.info("classB: {}", classB);
+        log.info("classC: {}", classC);
+        log.info("classD: {}", classD);
+        log.info("classE: {}", classE);
+    }
+}
